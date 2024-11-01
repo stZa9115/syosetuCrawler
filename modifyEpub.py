4ef559fe2379e579e49dd208a69a547a40928f3b
stZa9115 <s9706957@gmail.com>
2024-11-01 13:43:09 +0800

    change orderList to unorderList in toc

--- modifyEpub.py
+++ modifyEpub.py
@@ -1,19 +1,35 @@
+import zipfile
+import shutil
+import os
 import ebooklib
 from ebooklib import epub
-import zipfile
-from lxml import etree
 
-def convert_ol_to_ul(epub_file):
-    book = epub.read_epub(epub_file+'.epub')
-
-    for item in book.toc:
-        content = item.content
-        content = content.replace("<ol>", "<ul>", 1)  # Replace the first occurrence
-        content = content.replace("<li type=", "<li", 1)  # Remove the first type attribute
-        item.content = content
-
-    epub.save(epub_file+'v1.epub')
-    
 bookName = '転生王女と天才令嬢の魔法革命【Web版】'
 
-convert_ol_to_ul(bookName)
+os.makedirs('tmp')
+zip_ref = zipfile.ZipFile(bookName+'.epub')
+zip_ref.extractall('./tmp')
+
+file = open('./tmp/EPUB/nav.xhtml','r',encoding='utf-8')
+originText = file.read()
+newText = originText.replace('<ol>','<ul>')
+newText = newText.replace('</ol>','</ul>')
+file.close()
+
+file = open('./tmp/EPUB/nav.xhtml','w',encoding='utf-8')
+file.write(newText)
+file.close()
+###################
+def dfs(path,zf):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filePath = os.path.join(root,file)
+            newpath = filePath.replace('./tmp\\','.\\')
+            #print(os.path.join(root, file))
+            zf.write(filePath,newpath)
+
+reZip = zipfile.ZipFile('test.zip','w')
+dfs('./tmp',reZip)
+reZip.close()
+
+os.rename('test.zip',bookName+'_v2.epub')
